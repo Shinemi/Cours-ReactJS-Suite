afc8a3985dbb900647785962c7e8d77ad45c14cd
Shinemi <simon.lucas.83136@gmail.com>
2026-03-24 10:30:17 +0100

    added new modal window

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import Counter from "./components/counter";
 import Formulaire from "./components/formulaire";
+import { Modal } from "./components/modale";
 import Users from "./components/users";
 
 function App (){
+  const [isOpen, setIsOpen] = useState(false)
+
   return(
     <>     
       <h1>Test</h1>
       <Counter /> 
       <Formulaire/>
-      <Users />
+      {/* <Users /> */}
+
+      <button onClick={() => setIsOpen(true)}>
+        ouvrir la modale
+      </button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen (false)}>
+        <h2>Ma modale</h2>
+        <p>contenu de ma modale</p>
+      </Modal>
     </>
 
   )
